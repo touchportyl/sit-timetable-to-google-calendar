e3652dc4ef5bcd79b6176f3d2bed5e8fc9630fda
touchportal <touchportalofficial@gmail.com>
2023-08-23 00:00:50 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# sit-timetable-to-google-calendar
- 
+# Problem Statement
+Manually extract the timetable as HTML from the IN4SIT webpage and parse it into a csv.
+The file generated should be a .csv which can be imported into Google Calendar.
+
+# How to use
+1. Log into IN4SIT
+2. Open the shortcut
+3. Open Inspect Element (CTRL + SHIFT + I)
+4. Copy the <body> and paste it in 'source.txt'
+5. Run convert.py (double click)
+6. Import 'output.csv' into Google Calendar
+
+# How it works
+0. Magic
+1. Clean up all the useless HTML tags and keeps the important bits (run spellchecks here as well)
+2. Split data by course, then do further cleaning to remove headers and labels
+3. Extract important info into their own global variables
+4. Use the variables to construct a calendar event (in .csv format)
+5. Merge all the lists and output as a .csv file
+
+# Alternatives (but they didn't work for me lolz)
+[SIT Timetable Grabber](https://chrome.google.com/webstore/detail/sit-timetable-grabber/cnffedmfildfgejcckjcmhabbdkpcibh/)
+[Timetable Grabber - SIT](https://github.com/JustBrandonLim/timetable-grabber-sit)
+
+# Credits
+I made it. Thanks to stackoverflow and 
+
+---
+
+# dev notes
+
+## CSV Headers
+! Remember: Surround data with " if it has commas
+
+Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private
+
+Final exam,05/30/2020,10:00 AM,05/30/2020,1:00 PM,False,50 multiple choice questions and two essay questions,"Columbia, Schermerhorn 614",True
+
+
+## data reference
+
+<course_info> // breaks down into <course_id> - <name_of_course>
+
+<Status> // can just ignore, it's all probably enrolled
+<Units> // number up to 2dp for some reason
+<Grading> // graded or pass/fail
+
+// at the start of each component
+<Class_Nbr> // unique id for the class
+<Section> // `ALL` or a letter-number pair like `P4`, L-Lecture / P-Laboratory / W-Workshop
+<Component> // Lecture/Laboratory/Workshop
+
+// repeats
+<Days_and_Times> // time of the class
+<Room> // usually the same, Online or room id in the form of <campus>-<room_id> like `SP-SR2E`
+<Instructor> // name of instructor in full caps, trailing ` .` to clean up
+<Start/End_Date> // formatted as `DD/MM/YYYY - DD/MM/YYYY`
---
+++ convert.py
@@ -0,0 +1,192 @@
+from os.path import join, dirname, abspath
+from re import sub, search
+
+# regex
+regex1 = r'<[^<>]*>'  # selects all <> tags
+regex2 = r'&amp;'
+regex3 = r'[A-Z]{3} [0-9]{4}.*(?= -)' # selects course ids "CSD 1101" or "CSD 4902A"
+regex4 = r'Status|Units|Grading|Deadlines|Class Nbr|Section|Component|Days and Times|Room|Instructor|Start/End Date'
+regex5 = r'Graded|(Pass / Fail)' # get graded status
+regex6 = r'ALL|[LPW][0-9]' # get section
+regex7 = r'[MTWFS][ouehra] [0-2][0-9]:[0-5][0-9] - [0-2][0-9]:[0-5][0-9]' # get time
+
+# run any spellchecks here
+def Spellcheck():
+  line = line.replace('Integrated Work Study Programm', 'Integrated Work Study Programme')
+
+
+
+
+
+# source
+sourcepath = join(dirname(abspath(__file__)),'source.txt')
+with open(sourcepath) as f:
+  sourcefile = f.readlines()
+
+
+# cleanup
+cleanfile = []
+ignore = True
+for line in sourcefile:
+  # regex
+  line = sub(regex1, '', line)
+  line = sub(regex2, 'and', line)
+
+  # spellcheck
+  Spellcheck()
+
+  # ignore top lines until "Select Display Option"
+  if line == 'Select Display Option\n':
+    ignore = False
+
+  # ignore bottom lines after "Printer Friendly Page"
+  if line == 'Printer Friendly Page\n':
+    ignore = True
+  
+  # skip ahead
+  if ignore:
+    continue
+
+  # clean lines
+  if line != '\n' and line != '&nbsp;\n':
+    # append
+    cleanfile.append(line)
+
+# surgical post-cleanup cleaning
+datareadyfile = cleanfile[2:]
+for i in range(1,6):
+  del cleanfile[1]
+
+
+# splits all the courses into their own array
+# pre-extracts course info
+courses = ['ignore']
+datareadyfiles = []
+datareadyfile = []
+for line in cleanfile:
+  # match course id
+  courseid = search(regex3, line)
+
+  # catch every line in outputfile until the next time courseid matches
+  # this splits all the courses into their own array
+  if courseid is not None:
+    courses.append(courseid.string.rstrip())
+    datareadyfiles.append(datareadyfile)
+    datareadyfile = []
+    #outputfile.append(line)
+    continue
+  
+  datareadyfile.append(line)
+
+datareadyfiles.append(datareadyfile)
+
+# course header/label cleanup
+for index, file in enumerate(datareadyfiles):
+  for index, line in enumerate(file):
+    # clean up headers
+    search4 = search(regex4, line)
+    if search4 is not None:
+      file[index] = ''
+
+
+
+# data init for extraction
+csvlines = []
+csvline = []
+
+name_of_course = ''
+component = ''
+course_info = ''
+grading = ''
+section = ''
+instructor = ''
+start_date = ''
+start_time = ''
+end_date = ''
+end_time = ''
+location = ''
+
+# csv construction
+def ConstructCSVLine():
+  # build subject
+  subject = f'{name_of_course} ({component})'
+
+  # build description
+  description = f'<u><b>{course_info}</b></u><br>{grading} - {section} - {instructor}'
+  #<u><b>CSD 1101 - Computer Environment</b></u><br>Graded - <span>ALL - </span><span>VADIM SUROV</span>
+
+  # fill in the rest (All Day Event / Private)
+  all_day_event = False
+  private = True
+
+  # build csv line
+  #return ','.join(subject, start_date, start_time, end_date, end_time, all_day_event, description, location, private)
+  return f'{subject},{start_date},{start_time},{end_date},{end_time},{all_day_event},{description},{location},{private}\n'
+
+
+
+# data extraction
+for index, file in enumerate(datareadyfiles):
+  # ignore first file
+  if index == 0: continue
+
+  # get course info
+  course_info = courses[index]
+  ## get name of course
+  name_of_course = course_info.split(' - ')[1]
+
+  # start looking through
+  for index, line in enumerate(file):
+    # get grading (one per file)
+    search5 = search(regex5, line)
+    if search5 is not None:
+      grading = line.rstrip()
+    
+    if grading == '': continue
+    
+    # get section (start component block)
+    # get component
+    search6 = search(regex6, line)
+    if search6 is not None:
+      section = line.rstrip()
+      component = file[index+1].rstrip()
+    
+    if section == '': continue
+
+    # get time (start class block)
+    search7 = search(regex7, line)
+    if search7 is not None:
+      time = line.rstrip()
+      # split
+      start_time = time[3:8]
+      end_time = time[-5:]
+
+      # get room
+      location = file[index+1].rstrip()
+      # get instructor
+      instructor = file[index+2].rstrip().replace(' .','')
+
+      # get date
+      date = file[index+3].rstrip().split(' - ')
+      # split
+      start_date = date[0]
+      end_date = date[1]
+
+      # for each class block, write a csvline with data from above
+      csvline = ConstructCSVLine()
+      csvlines.append(csvline)
+
+
+
+# write to output
+# init with header
+outputfile = ['Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private\n']
+for csvline in csvlines:
+  for line in csvline:
+    outputfile.append(line)
+
+
+# write to csv
+outputpath = join(dirname(abspath(__file__)),'output.csv')
+with open(outputpath, "w") as f:
+  f.writelines(outputfile)
